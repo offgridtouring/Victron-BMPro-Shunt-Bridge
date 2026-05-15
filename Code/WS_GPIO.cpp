773ff6cc7c0e7622e8dd11ada863ad3c7862f5e9
offgridtouring <gary@offgridtouring.au>
2026-05-15 20:47:00 +1000

    Delete Code directory

--- Code/WS_GPIO.cpp
+++
@@ -1,8 +0,0 @@
-#include "WS_GPIO.h"
-
-/*************************************************************  I/O Init  *************************************************************/
-void digitalToggle(int pin)
-{
-  digitalWrite(pin, !digitalRead(pin));                                         // Toggle the state of the pin
-}
-
--- Code/WS_Information.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define Extension_Enable      1                   // Whether to extend the connection to external devices     1:Expansion device Modbus RTU Relay     0:No extend
-#define RTC_Event_Enable      1                   // Whether to enable RTC events  (Bluetooth)                1:Enable                                0:Disable
-
-
